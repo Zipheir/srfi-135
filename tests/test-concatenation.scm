3c18056e28b2839d724c0fa117db695dbc326d48
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2021-09-04 12:31:54 -0400

    Fix mistakes in concatenations test group.
    We can't use `test` to compare strings and texts; we need the utility
    `result=?` function.

--- tests/test-concatenation.scm
+++ tests/test-concatenation.scm
@@ -1,17 +1,19 @@
 (test-group "Concatenation"
-  (test "" (textual-append))
+  (test-assert (result=? "" (textual-append)))
 
-  (test "abcdef"
-        (textual-append (as-text "")
-                        (as-text "a")
-                        (as-text "bcd")
-                        "" "ef" "" ""))
+  (test-assert
+   (result=? "abcdef"
+             (textual-append (as-text "")
+                             (as-text "a")
+                             (as-text "bcd")
+                             "" "ef" "" "")))
 
-  (test "" (textual-concatenate '()))
+  (test-assert (result=? "" (textual-concatenate '())))
 
-  (test "abcdef"
-        (textual-concatenate
-         (map string->text '("" "a" "bcd" "" "ef" "" ""))))
+  (test-assert
+   (result=? "abcdef"
+             (textual-concatenate
+              (map string->text '("" "a" "bcd" "" "ef" "" "")))))
 
   ;;; textual-concatenate is likely to have special cases for longer texts.
 
@@ -27,97 +29,119 @@
          (t100-500 (subtext alph100 100 500))
          (t600-999 (subtext alph100 600 999)))
 
-    (test str10 alph10)
-    (test str100 alph100)
-    (test str100-500 t100-500)
-    (test str600-999 t600-999)
+    (test-assert (result=? str10 alph10))
+    (test-assert (result=? str100 alph100))
+    (test-assert (result=? str100-500 t100-500))
+    (test-assert (result=? str600-999 t600-999))
 
     ;; concatenating a short text with a long text
 
-    (test (string-append str1 str600-999)
-          (textual-concatenate (list alph1 t600-999)))
+    (test-assert
+     (result=? (string-append str1 str600-999)
+               (textual-concatenate (list alph1 t600-999))))
 
-    (test (string-append str1 str600-999)
-          (textual-concatenate (list alph1 (textual-copy t600-999))))
+    (test-assert
+     (result=? (string-append str1 str600-999)
+               (textual-concatenate (list alph1 (textual-copy t600-999)))))
 
-    (test (string-append str600-999 str1)
-          (textual-concatenate (list t600-999 alph1)))
+    (test-assert
+     (result=? (string-append str600-999 str1)
+               (textual-concatenate (list t600-999 alph1))))
 
-    (test (string-append str600-999 str1)
-          (textual-concatenate (list (textual-copy t600-999) alph1)))
+    (test-assert
+     (result=? (string-append str600-999 str1)
+               (textual-concatenate (list (textual-copy t600-999) alph1))))
     )
 
 
-  (test "" (textual-concatenate-reverse '()))
+  (test-assert (result=? "" (textual-concatenate-reverse '())))
 
-  (test "efbcda"
-        (textual-concatenate-reverse
-         (map string->text '("" "a" "bcd" "" "ef" "" ""))))
+  (test-assert
+   (result=? "efbcda"
+             (textual-concatenate-reverse
+              (map string->text '("" "a" "bcd" "" "ef" "" "")))))
 
-  (test "huh?" (textual-concatenate-reverse '() "huh?"))
+  (test-assert (result=? "huh?" (textual-concatenate-reverse '() "huh?")))
 
-  (test "efbcdaxy"
-        (textual-concatenate-reverse '("" "a" "bcd" "" "ef" "" "") "xy"))
+  (test-assert
+   (result=? "efbcdaxy"
+             (textual-concatenate-reverse '("" "a" "bcd" "" "ef" "" "") "xy")))
 
-  (test "huh" (textual-concatenate-reverse '() "huh?" 3))
+  (test-assert (result=? "huh" (textual-concatenate-reverse '() "huh?" 3)))
 
-  (test "efbcdax"
-        (textual-concatenate-reverse
-         '("" "a" "bcd" "" "ef" "" "") "x" 1))
+  (test-assert
+   (result=? "efbcdax"
+             (textual-concatenate-reverse '("" "a" "bcd" "" "ef" "" "") "x" 1)))
 
 
-  (test "" (textual-join '()))
+  (test-assert (result=? "" (textual-join '())))
 
-  (test " ab cd  e f "
-        (textual-join (map string->text '("" "ab" "cd" "" "e" "f" ""))))
+  (test-assert
+   (result=? " ab cd  e f "
+             (textual-join (map string->text '("" "ab" "cd" "" "e" "f" "")))))
 
-  (test "" (textual-join '() ""))
+  (test-assert (result=? "" (textual-join '() "")))
 
-  (test "abcdef" (textual-join '("" "ab" "cd" "" "e" "f" "") ""))
+  (test-assert
+   (result=? "abcdef" (textual-join '("" "ab" "cd" "" "e" "f" "") "")))
 
-  (test "" (textual-join '() "xyz"))
+  (test-assert (result=? "" (textual-join '() "xyz")))
 
-  (test "xyzabxyzcdxyzxyzexyzfxyz"
-        (textual-join '("" "ab" "cd" "" "e" "f" "") "xyz"))
+  (test-assert
+   (result=? "xyzabxyzcdxyzxyzexyzfxyz"
+             (textual-join '("" "ab" "cd" "" "e" "f" "") "xyz")))
 
-  (test "" (textual-join '() "" 'infix))
+  (test-assert (result=? "" (textual-join '() "" 'infix)))
 
-  (test "abcdef" (textual-join '("" "ab" "cd" "" "e" "f" "") "" 'infix))
+  (test-assert
+   (result=? "abcdef"
+             (textual-join '("" "ab" "cd" "" "e" "f" "") "" 'infix)))
 
-  (test "" (textual-join '() "xyz" 'infix))
+  (test-assert (result=? "" (textual-join '() "xyz" 'infix)))
 
-  (test "xyzabxyzcdxyzxyzexyzfxyz"
-        (textual-join '("" "ab" "cd" "" "e" "f" "") (as-text "xyz") 'infix))
+  (test-assert
+   (result=? "xyzabxyzcdxyzxyzexyzfxyz"
+             (textual-join '("" "ab" "cd" "" "e" "f" "")
+                           (as-text "xyz")
+                           'infix)))
 
   (test 'horror
         (guard (exn (#t 'horror))
           (textual-join '() "" 'strict-infix)))
 
-  (test "abcdef"
-        (textual-join '("" "ab" "cd" "" "e" "f" "") "" 'strict-infix))
+  (test-assert
+   (result=? "abcdef"
+             (textual-join '("" "ab" "cd" "" "e" "f" "") "" 'strict-infix)))
 
   (test 'wham
         (guard (exn (else 'wham))
           (textual-join '() "xyz" 'strict-infix)))
 
-  (test "xyzabxyzcdxyzxyzexyzfxyz"
-        (textual-join '("" "ab" "cd" "" "e" "f" "") "xyz" 'strict-infix))
+  (test-assert
+   (result=? "xyzabxyzcdxyzxyzexyzfxyz"
+             (textual-join '("" "ab" "cd" "" "e" "f" "") "xyz" 'strict-infix)))
 
-  (test "" (textual-join '() "" 'suffix))
+  (test-assert (result=? "" (textual-join '() "" 'suffix)))
 
-  (test "abcdef" (textual-join '("" "ab" "cd" "" "e" "f" "") "" 'suffix))
+  (test-assert
+   (result=? "abcdef"
+             (textual-join '("" "ab" "cd" "" "e" "f" "") "" 'suffix)))
 
-  (test "" (textual-join '() "xyz" 'suffix))
+  (test-assert (result=? "" (textual-join '() "xyz" 'suffix)))
 
-  (test "xyzabxyzcdxyzxyzexyzfxyzxyz"
-        (textual-join '("" "ab" "cd" "" "e" "f" "") "xyz" 'suffix))
+  (test-assert
+   (result=? "xyzabxyzcdxyzxyzexyzfxyzxyz"
+             (textual-join '("" "ab" "cd" "" "e" "f" "") "xyz" 'suffix)))
 
-  (test "" (textual-join '() "" 'prefix))
+  (test-assert (result=? "" (textual-join '() "" 'prefix)))
 
-  (test "abcdef" (textual-join '("" "ab" "cd" "" "e" "f" "") "" 'prefix))
+  (test-assert
+   (result=? "abcdef"
+             (textual-join '("" "ab" "cd" "" "e" "f" "") "" 'prefix)))
 
-  (test "" (textual-join '() "xyz" 'prefix))
+  (test-assert (result=? "" (textual-join '() "xyz" 'prefix)))
 
-  (test "xyzxyzabxyzcdxyzxyzexyzfxyz"
-        (textual-join '("" "ab" "cd" "" "e" "f" "") "xyz" 'prefix))
-
+  (test-assert
+   (result=? "xyzxyzabxyzcdxyzxyzexyzfxyz"
+             (textual-join '("" "ab" "cd" "" "e" "f" "") "xyz" 'prefix)))
+  )
