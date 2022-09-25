9d20400f21ae94108a682709c79c19db62a786c8
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-09-25 14:22:18 -0400

    Re-indent files.

--- exceptions.scm
+++ exceptions.scm
@@ -1,47 +1,47 @@
-  (define-syntax assert-type
-    (syntax-rules ()
-      ((assert-type loc expr)
-       (unless expr
-         (abort
-          (make-composite-condition
-           (make-property-condition 'exn
-            'location loc
-            'message "type check failed"
-            'arguments (list 'expr))
-           (make-property-condition 'type)
-           (make-property-condition 'assertion)))))))
+(define-syntax assert-type
+  (syntax-rules ()
+    ((assert-type loc expr)
+     (unless expr
+       (abort
+        (make-composite-condition
+         (make-property-condition 'exn
+          'location loc
+          'message "type check failed"
+          'arguments (list 'expr))
+         (make-property-condition 'type)
+         (make-property-condition 'assertion)))))))
 
-  (define (arity-exception loc argl)
-    (abort
-     (make-composite-condition
-      (make-property-condition 'exn
-       'location loc
-       'message "invalid number of arguments"
-       'arguments argl)
-      (make-property-condition 'arity)
-      (make-property-condition 'assertion))))
+(define (arity-exception loc argl)
+  (abort
+   (make-composite-condition
+    (make-property-condition 'exn
+     'location loc
+     'message "invalid number of arguments"
+     'arguments argl)
+    (make-property-condition 'arity)
+    (make-property-condition 'assertion))))
 
-  (define (bounds-exception loc msg . args)
-    (abort
-     (make-composite-condition
-      (make-property-condition 'exn
-       'location loc
-       'message msg
-       'arguments args)
-      (make-property-condition 'bounds)
-      (make-property-condition 'assertion))))
+(define (bounds-exception loc msg . args)
+  (abort
+   (make-composite-condition
+    (make-property-condition 'exn
+     'location loc
+     'message msg
+     'arguments args)
+    (make-property-condition 'bounds)
+    (make-property-condition 'assertion))))
 
-  (define (%check-index loc t i)
-    (unless (<= 0 i (%textual-length-no-checks t))
-      (bounds-exception loc "index out of bounds" i t)))
+(define (%check-index loc t i)
+  (unless (<= 0 i (%textual-length-no-checks t))
+    (bounds-exception loc "index out of bounds" i t)))
 
-  (define (%check-range loc t start end)
-    (unless (<= 0 start end (%textual-length-no-checks t))
-      (bounds-exception loc
-                        "invalid range"
-                        start
-                        end
-                        t)))
+(define (%check-range loc t start end)
+  (unless (<= 0 start end (%textual-length-no-checks t))
+    (bounds-exception loc
+                      "invalid range"
+                      start
+                      end
+                      t)))
 
 ;;; Same things, but for bytevectors.
 
--- util.scm
+++ util.scm
@@ -1,24 +1,24 @@
-  ;; Utility
-  (define (complain name . args)
-    (apply error
-           (string-append (symbol->string name) ": illegal arguments")
-           args))
+;; Utility
+(define (complain name . args)
+  (apply error
+         (string-append (symbol->string name) ": illegal arguments")
+         args))
 
-  (: list->bytevector ((list-of fixnum) -> bytevector))
-  (define (list->bytevector bytes)
-    (let* ((n (length bytes))
-           (bv (make-bytevector n)))
-      (do ((i 0 (+ i 1))
-           (bytes bytes (cdr bytes)))
-          ((= i n))
-        (bytevector-u8-set! bv i (car bytes)))
-      bv))
+(: list->bytevector ((list-of fixnum) -> bytevector))
+(define (list->bytevector bytes)
+  (let* ((n (length bytes))
+         (bv (make-bytevector n)))
+    (do ((i 0 (+ i 1))
+         (bytes bytes (cdr bytes)))
+        ((= i n))
+      (bytevector-u8-set! bv i (car bytes)))
+    bv))
 
-  (define (pair-or-null? x)
-    (or (null? x) (pair? x)))
+(define (pair-or-null? x)
+  (or (null? x) (pair? x)))
 
-  (define (exact-natural? x)
-    (and (exact-integer? x) (>= x 0)))
+(define (exact-natural? x)
+  (and (exact-integer? x) (>= x 0)))
 
 (define (%textual-or-char? x)
   (or (string? x) (text? x) (char? x)))
