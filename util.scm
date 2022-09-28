907e7df86509a4914fd7d097f5bde5f634e76009
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-09-27 22:24:44 -0400

    Remove unused utility procedure.

--- util.scm
+++ util.scm
@@ -4,16 +4,6 @@
          (string-append (symbol->string name) ": illegal arguments")
          args))
 
-(: list->bytevector ((list-of fixnum) -> bytevector))
-(define (list->bytevector bytes)
-  (let* ((n (length bytes))
-         (bv (make-bytevector n)))
-    (do ((i 0 (+ i 1))
-         (bytes bytes (cdr bytes)))
-        ((= i n))
-      (bytevector-u8-set! bv i (car bytes)))
-    bv))
-
 (define (pair-or-null? x)
   (or (null? x) (pair? x)))
 
