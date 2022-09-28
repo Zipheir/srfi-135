d61b9c18b059f093e846da3382f951aa7a4f3458
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-09-27 22:29:14 -0400

    Types for utility procedures.

--- util.scm
+++ util.scm
@@ -1,11 +1,15 @@
 ;; Utility
+
+(: complain (symbol #!rest -> noreturn))
 (define (complain name . args)
   (apply error
          (string-append (symbol->string name) ": illegal arguments")
          args))
 
+(: pair-or-null? (* -> boolean))
 (define (pair-or-null? x)
   (or (null? x) (pair? x)))
 
+(: natural-fixnum? (* -> boolean))
 (define (natural-fixnum? x)
   (and (fixnum? x) (>= x 0)))
